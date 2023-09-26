3b5994644cded07847c72ee4e1416fb06db7edeb
hariom <hariompatidar32498@gmail.com>
2023-09-26 18:51:05 +0530

    update mailer view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      # UserMailer.with(user: user).welcome_email.deliver_now
+      UserMailer.with(user: user).welcome_email.deliver_now
       render json: user, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email
+    byebug
     @user = params[:user]
     mail(to: @user.email, subject: 'Welcome TO Uber_Eats !!')
   end
