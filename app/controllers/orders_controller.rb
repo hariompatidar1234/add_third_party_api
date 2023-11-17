3ada38e3f052018a85dbeb9281e6491ff41f2da7
hariom patidar <hariompatidar32498.com>
2023-11-17 18:37:09 +0530

    create order view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,40 +6,63 @@ class OrdersController < ApplicationController
     render json: @orders, status: :ok
   end
 
+  # def create
+  #   @cart = current_user.cart
+  #   if @cart.cart_items.empty?
+  #     render json: { error: 'Cart is empty. Cannot create an order with an empty cart.' },status: :unprocessable_entity
+  #   else
+  #     @order = current_user.orders.new(address: params[:address])
+
+  #     if @order.save
+  #       OrderMailer.order_confirm(@current_user).deliver_now
+  #       create_order_items(@order)
+  #       render json: { data: @order, message: 'Order created successfully!' },
+  #       status: :created
+  #     else
+  #       render json: { error: @order.errors.full_messages }, status: :unprocessable_entity
+  #     end
+  #   end
+  # end
+
+  
+
+  # def create_order_items(order)
+  #   cart_items = current_user.cart.cart_items.includes(:dish)
+  #   cart_items.each do |cart_item|
+  #     order_item = order.order_items.new(
+  #       dish_id: cart_item.dish.id,
+  #       quantity: cart_item.quantity,
+  #       total_amount: cart_item.quantity * cart_item.dish.price
+  #     )
+  #     order_item.save
+  #   end
+  #   current_user.cart.cart_items.destroy_all
+  # end
+
   def create
     @cart = current_user.cart
     if @cart.cart_items.empty?
-      render json: { error: 'Cart is empty. Cannot create an order with an empty cart.' },status: :unprocessable_entity
+      flash[:alert] = 'Cart is empty. Cannot create an order with an empty cart.'
+      redirect_to root_path
     else
       @order = current_user.orders.new(address: params[:address])
-
+  
       if @order.save
-        OrderMailer.order_confirm(@current_user).deliver_now
+        OrderMailer.order_confirm(current_user).deliver_now
         create_order_items(@order)
-        render json: { data: @order, message: 'Order created successfully!' },
-        status: :created
+        flash[:notice] = 'Order created successfully!'
+        redirect_to @order
       else
-        render json: { error: @order.errors.full_messages }, status: :unprocessable_entity
+        flash[:error] = @order.errors.full_messages.join(', ')
+        render 'new' # Render the new order form again
       end
     end
   end
+  
 
   def show
     @orders = current_user.orders
     render json: @orders, status: :ok
-  end
-
-  def create_order_items(order)
-    cart_items = current_user.cart.cart_items.includes(:dish)
-    cart_items.each do |cart_item|
-      order_item = order.order_items.new(
-        dish_id: cart_item.dish.id,
-        quantity: cart_item.quantity,
-        total_amount: cart_item.quantity * cart_item.dish.price
-      )
-      order_item.save
-    end
-    current_user.cart.cart_items.destroy_all
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,49 @@
-class UsersController < ApplicationController
-  # before_action :authenticate_request, except: %i[create login forgot_password reset_password]
+  class UsersController < ApplicationController
+    # before_action :authenticate_request, except: %i[create login forgot_password reset_password]
 
-  def index
-    @users =  User.all
-  end
+    def index
+      @users =  User.all
+    end
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      # UserMailer.with(user: user).welcome_email.deliver_now
-       # flash.notice = 'the account  successfully saved'
-      render json: user, status: :created
-    else
-      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    def create
+      user = User.new(user_params)
+      if user.save
+        # UserMailer.with(user: user).welcome_email.deliver_now
+        # flash.notice = 'the account  successfully saved'
+        render json: user, status: :created
+      else
+        render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      @current_user
+    end
+
+    def update
+      if @current_user.update(user_params)
+        render json: { data: @current_user, message: 'User updated' }
+      else
+        render json: { error: @current_user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+
+    def destroy
+      if @current_user.destroy
+        render json: { data: @current_user,message: 'User deleted' }
+      else
+        render json: { error: 'User deletion failed' }
+      end
+    end
+
+
+    private
+
+    def user_params
+      params.require(:user).  permit(:name, :email, :password, :type,:picture)
     end
   end
-
-  def show
-    @current_user
-  end
-
-  def update
-    if @current_user.update(user_params)
-      render json: { data: @current_user, message: 'User updated' }
-    else
-      render json: { error: @current_user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-
-  def destroy
-    if @current_user.destroy
-      render json: { data: @current_user,message: 'User deleted' }
-    else
-      render json: { error: 'User deletion failed' }
-    end
-  end
-
-
-  private
-
-  def user_params
-    params.require(:user).  permit(:name, :email, :password, :type,:picture)
-  end
-end
 
 
 
